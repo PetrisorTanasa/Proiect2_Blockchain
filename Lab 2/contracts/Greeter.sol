1185ba1fd98d5272f1128ab712535e8d4624c990
PetrisorTanasa <fptanasa02@yahoo.com>
2022-04-12 11:45:23 +0300

    .

--- Lab 2/contracts/Greeter.sol
+++ Lab 2/contracts/Greeter.sol
@@ -1,4 +1,4 @@
-
+//SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
@@ -8,8 +8,10 @@ contract Greeter {
     string private greeting;
     IUniswapV2Router01 tokenAddress;
     IUniswapV2Router01 router= IUniswapV2Router01(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
+    address private constant _usdt = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
 
-    constructor() public {}
+    constructor() {}
+    
     function swapETHToUSDT() public payable {
     address[] memory path = new address[](2);
     path[0] = router.WETH();
@@ -21,4 +23,5 @@ contract Greeter {
             msg.sender,
             block.timestamp + 100
         );
+}
 }
